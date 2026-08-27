4c8d7bbf2ca2b5d25f3835c6b653bb813312e670
ckluetmeier <ckluetmeier@gmail.com>
2026-08-27 16:24:35 -0400

    comments

--- 4.0_comparison_helpers.R
+++ 4.0_comparison_helpers.R
@@ -324,7 +324,7 @@ version_change <- function(df, value_col, id_col = "id_harmonised") {
 
 
 # -----------------------------------------------------------------------------
-# Plotting stuff
+# Plotting constants
 # -----------------------------------------------------------------------------
 river_levels  <- c("CL", "SJ", "CD", "PR", "upperYR", "lowerYR")
 river_labels  <- c("Coleen", "Sheenjek", "Chandalar", "Porcupine",
